9541b81a5f12efd19479140fd4962a02eb176880
aplejandro <i-vang@outlook.com>
2020-04-30 18:50:32 -0500

    Problem Set 3

---
+++ week3/tideman/tideman.c
@@ -0,0 +1,263 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Max number of candidates
+#define MAX 9
+
+// preferences[i][j] is number of voters who prefer i over j
+int preferences[MAX][MAX];
+
+// locked[i][j] means i is locked in over j
+bool locked[MAX][MAX];
+
+// Each pair has a winner, loser
+typedef struct
+{
+    int winner;
+    int loser;
+}
+pair;
+
+// Array of candidates
+string candidates[MAX];
+pair pairs[MAX * (MAX - 1) / 2];
+pair temporal[MAX * (MAX - 1) / 2];
+
+
+int pair_count;
+int candidate_count;
+
+int aux[MAX * (MAX - 1) / 2];
+
+// Function prototypes
+bool vote(int rank, string name, int ranks[]);
+void record_preferences(int ranks[]);
+void add_pairs(void);
+void sort_pairs(void);
+void lock_pairs(void);
+void print_winner(void);
+void insertionSort(int arr[], int n);
+void reverseArray(int arr[], int start, int end);
+bool loopcheck(int a, int b);
+
+
+int main(int argc, string argv[])
+{
+    // Check for invalid usage
+    if (argc < 2)
+    {
+        printf("Usage: tideman [candidate ...]\n");
+        return 1;
+    }
+
+    // Populate array of candidates
+    candidate_count = argc - 1;
+    if (candidate_count > MAX)
+    {
+        printf("Maximum number of candidates is %i\n", MAX);
+        return 2;
+    }
+    for (int i = 0; i < candidate_count; i++)
+    {
+        candidates[i] = argv[i + 1];
+    }
+
+    // Clear graph of locked in pairs
+    for (int i = 0; i < candidate_count; i++)
+    {
+        for (int j = 0; j < candidate_count; j++)
+        {
+            locked[i][j] = false;
+        }
+    }
+
+    pair_count = 0;
+    int voter_count = get_int("Number of voters: ");
+
+    // Query for votes
+    for (int i = 0; i < voter_count; i++)
+    {
+        // ranks[i] is voter's ith preference
+        int ranks[candidate_count];
+
+        // Query for each rank
+        for (int j = 0; j < candidate_count; j++)
+        {
+            string name = get_string("Rank %i: ", j + 1);
+
+            if (!vote(j, name, ranks))
+            {
+                printf("Invalid vote.\n");
+                return 3;
+            }
+        }
+
+        record_preferences(ranks);
+
+        printf("\n");
+    }
+
+    add_pairs();
+    for (int h = 0; h < pair_count ; h++){
+        printf("%d-%d  %d-%d\n", pairs[h].winner,pairs[h].loser,preferences[pairs[h].winner][pairs[h].loser],preferences[pairs[h].loser][pairs[h].winner]);
+    }
+    sort_pairs();
+    for (int h = 0; h < pair_count ; h++){
+        printf("%d\n",aux[h]);
+    }
+    lock_pairs();
+    print_winner();
+    return 0;
+}
+
+// Update ranks given a new vote
+bool vote(int rank, string name, int ranks[])
+{
+    // TODO
+    for (int i = 0 ; i < candidate_count ; i++)
+    {
+        if (strcmp(candidates[i],name)==0)
+        {
+            ranks[rank] = i;
+            printf("Ranks[%d] = %d\n",ranks[i],i);
+            return true;
+        }
+    }
+    return false;
+}
+
+// Update preferences given one voter's ranks
+void record_preferences(int ranks[])
+{
+    for (int i = 0; i < candidate_count; i++)
+    {
+        for (int j = i + 1; j < candidate_count; j++)
+        {
+            preferences[ranks[i]][ranks[j]]++;
+        }
+    }
+}
+
+// Record pairs of candidates where one is preferred over the other
+void add_pairs(void)
+{
+    for (int i = 0; i < candidate_count; i++)
+    {
+        for (int j = i + 1; j < candidate_count; j++)
+        {
+            if (preferences[i][j] > preferences[j][i])
+            {
+                pairs[pair_count].winner = i;
+                pairs[pair_count].loser = j;
+                pair_count++;
+            }
+            else if (preferences[i][j] < preferences[j][i])
+            {
+                pairs[pair_count].winner = j;
+                pairs[pair_count].loser = i;
+                pair_count++;
+            }
+        }
+    }
+}
+
+
+void insertionSort(int arr[], int n)
+{
+    int i, key, j;
+    for (i = 1; i < n; i++) {
+        key = arr[i];
+        j = i - 1;
+
+        /* Move elements of arr[0..i-1], that are
+          greater than key, to one position ahead
+          of their current position */
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+// Sort pairs in decreasing order by strength of victory
+void sort_pairs(void)
+{
+    int nswaps = -1;
+    pair temp;
+    do
+    {
+        nswaps = 0;
+        for (int i = 0; i < pair_count - 1; i++)
+        {
+            if (preferences[pairs[i].winner][pairs[i].loser] < preferences[pairs[i+1].winner][pairs[i+1].loser])
+            {
+                temp = pairs[i];
+                pairs[i] = pairs[i+1];
+                pairs[i+1] = temp;
+
+                nswaps++;
+            }
+        }
+    }
+    while (nswaps != 0);
+
+}
+
+bool loopcheck(int a, int b)
+{
+    if (locked[b][a] == true)
+    {
+        return true;
+    }
+
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (locked[i][a] == true)
+        {
+            return loopcheck(i, b);
+        }
+    }
+    return false;
+}
+
+// Lock pairs into the candidate graph in order, without creating cycles
+void lock_pairs(void)
+{
+    for (int i = 0; i < pair_count; i++)
+    {
+        if (!loopcheck(pairs[i].winner, pairs[i].loser))
+        {
+            locked[pairs[i].winner][pairs[i].loser] = true;
+        }
+    }
+    return;
+}
+
+// Print the winner of the election
+void print_winner(void)
+{
+    int winner;
+    int rank;
+
+    for (int i = 0; i < candidate_count; i++)
+    {
+        rank = 0;
+        for (int k = 0; k < candidate_count; k++)
+        {
+            if (locked[k][i] == false)
+            {
+                rank++;
+            }
+        }
+
+        // Prints all the names that are the source of the graph
+        if (rank == candidate_count)
+        {
+            printf("%s\n", candidates[i]);
+        }
+    }
+}
+
